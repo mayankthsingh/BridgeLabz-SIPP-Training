1d54a370dd69cade93d89aeb609349f5062d6442
Mayank Singh <thakurmayanksingh208@gmail.com>
2025-06-27 10:05:56 +0530

    Oops proboems

--- ProgrammingElements/level1/HarryAge.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class HarryAge {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int BirthAge = sc.nextInt();
-        int CurrentAge=2024 - BirthAge;
-        System.out.println("Harry's age in 2024 is"+CurrentAge);
-    }
-}
