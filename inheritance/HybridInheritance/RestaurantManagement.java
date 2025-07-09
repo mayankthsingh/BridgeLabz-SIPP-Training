f632418a73960f2cdca6bca082f0c3b27688ca5c
Mayank Singh <thakurmayanksingh208@gmail.com>
2025-07-09 11:35:44 +0530

    Added inheritance

--- inheritance/HybridInheritance/RestaurantManagement.java
+++ inheritance/HybridInheritance/RestaurantManagement.java
@@ -1,12 +1,17 @@
-// Hybrid Inheritance (with Interface)
+
 
 class Person {
     String name;
     int id;
 
-    public Person(String name, int id) {
+    Person(String name, int id) {
         this.name = name;
         this.id = id;
+    }
+
+    void displayPersonInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
     }
 }
 
@@ -15,31 +20,43 @@ interface Worker {
 }
 
 class Chef extends Person implements Worker {
-    public Chef(String name, int id) {
+
+    Chef(String name, int id) {
         super(name, id);
     }
 
+    @Override
     public void performDuties() {
         System.out.println(name + " is cooking dishes.");
     }
 }
 
 class Waiter extends Person implements Worker {
-    public Waiter(String name, int id) {
+
+    Waiter(String name, int id) {
         super(name, id);
     }
 
+    @Override
     public void performDuties() {
         System.out.println(name + " is serving customers.");
     }
 }
 
-public class RestaurantSystem {
+public class RestaurantManagement {
     public static void main(String[] args) {
-        Chef chef = new Chef("Raj", 101);
-        Waiter waiter = new Waiter("Vikram", 102);
 
+        Chef chef = new Chef("Ravi", 101);
+        Waiter waiter = new Waiter("Anita", 201);
+
+        System.out.println("Chef Info:");
+        chef.displayPersonInfo();
         chef.performDuties();
+
+        System.out.println();
+
+        System.out.println("Waiter Info:");
+        waiter.displayPersonInfo();
         waiter.performDuties();
     }
 }
--- inheritance/SingleInheritance/LibraryManagement.java
+++ inheritance/SingleInheritance/LibraryManagement.java
@@ -1,12 +1,16 @@
-// Single Inheritance
 
 class Book {
     String title;
     int publicationYear;
 
-    public Book(String title, int publicationYear) {
+    Book(String title, int publicationYear) {
         this.title = title;
         this.publicationYear = publicationYear;
+    }
+
+    void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Publication Year: " + publicationYear);
     }
 }
 
@@ -14,23 +18,24 @@ class Author extends Book {
     String name;
     String bio;
 
-    public Author(String title, int publicationYear, String name, String bio) {
+    Author(String title, int publicationYear, String name, String bio) {
         super(title, publicationYear);
         this.name = name;
         this.bio = bio;
     }
 
-    public void displayInfo() {
-        System.out.println("Book Title: " + title);
-        System.out.println("Published Year: " + publicationYear);
+    @Override
+    void displayInfo() {
+        super.displayInfo();
         System.out.println("Author Name: " + name);
         System.out.println("Author Bio: " + bio);
     }
 }
 
-public class LibrarySystem {
+public class LibraryManagement {
     public static void main(String[] args) {
-        Author author = new Author("Wings of Fire", 1999, "A.P.J. Abdul Kalam", "Former President of India and Scientist");
+        
+        Author author = new Author("The Java Handbook", 2022, "John Doe", "Experienced Java Developer and Author.");
         author.displayInfo();
     }
 }
